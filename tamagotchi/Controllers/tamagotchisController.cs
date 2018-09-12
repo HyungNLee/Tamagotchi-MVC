a200b30b00c35245efacc66d016a1f2dee84f87f
Brian Nelson, Hyung Lee and Kenneth Du <hyung@>
2018-09-12 14:57:21 -0700

    Finish Tamagotchi web app. Need to fix naming conventions.

--- tamagotchi/Controllers/tamagotchisController.cs
+++ tamagotchi/Controllers/tamagotchisController.cs
@@ -26,5 +26,49 @@ namespace Tamagotchis.Controllers
       List<Tamagotchi> allToys = Tamagotchi.GetAll();
       return View("Show", allToys);
     }
+
+    [HttpGet("/show/{id}")]
+    public ActionResult Show(int id)
+    {
+      List<Tamagotchi> allToys = Tamagotchi.GetAll();
+      int find = id - 1;
+      return View("ShowID", allToys[find]);
+    }
+
+    [HttpPost("/show/{id}/feed")]
+    public ActionResult Feed(int id)
+    {
+      List<Tamagotchi> allToys = Tamagotchi.GetAll();
+      int find = id - 1;
+      allToys[find].Feed();
+      return View ("ShowID", allToys[find]);
+    }
+
+    [HttpPost("/show/{id}/play")]
+    public ActionResult Play(int id)
+    {
+      List<Tamagotchi> allToys = Tamagotchi.GetAll();
+      int find = id - 1;
+      allToys[find].Play();
+      return View ("ShowID", allToys[find]);
+    }
+
+    [HttpPost("/show/{id}/rest")]
+    public ActionResult Rest(int id)
+    {
+      List<Tamagotchi> allToys = Tamagotchi.GetAll();
+      int find = id - 1;
+      allToys[find].Rest();
+      return View ("ShowID", allToys[find]);
+    }
+
+    [HttpPost("/show/{id}/time")]
+    public ActionResult Time(int id)
+    {
+      List<Tamagotchi> allToys = Tamagotchi.GetAll();
+      int find = id - 1;
+      allToys[find].SimTime();
+      return View ("ShowID", allToys[find]);
+    }
   }
 }
--- tamagotchi/Models/tamagotchi.cs
+++ tamagotchi/Models/tamagotchi.cs
@@ -9,7 +9,10 @@ namespace Tamagotchis.Models
     public int Hunger {get; set;}
     public int Energy {get; set;}
     public int Attention {get; set;}
+    private int _id;
+    public string _picURL {get; set;}
     private static List<Tamagotchi> _toys = new List<Tamagotchi> {};
+    private static int picNumber = 1;
 
     public Tamagotchi(string name)
     {
@@ -18,26 +21,108 @@ namespace Tamagotchis.Models
       Energy = 100;
       Attention = 100;
       _toys.Add(this);
+      _id = _toys.Count;
+      if (picNumber < 6)
+      {
+        _picURL = GetPicture(picNumber);
+        picNumber++;
+      }
+      else
+      {
+        picNumber = 1;
+        _picURL = GetPicture(picNumber);
+        picNumber++;
+      }
+    }
+
+    public void SetHunger(int amount)
+    {
+      Hunger = amount;
+    }
+
+    public int GetHunger()
+    {
+      return Hunger;
     }
 
     public void Feed()
     {
       Hunger += 30;
+      if (Hunger > 100)
+      {
+        Hunger = 100;
+      }
     }
 
     public void Rest()
     {
       Energy += 30;
+      if (Energy > 100)
+      {
+        Energy = 100;
+      }
     }
 
     public void Play()
     {
       Attention += 30;
+      if (Attention > 100)
+      {
+        Attention = 100;
+      }
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public void SimTime()
+    {
+      Hunger -= 10;
+      Attention -= 6;
+      Energy -= 3;
+      if (CheckDead())
+      {
+        Console.WriteLine(Name + " is dead!");
+      }
+    }
+    public bool CheckDead()
+    {
+      if (Hunger == 0 || Attention == 0 || Energy == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public static List<Tamagotchi> GetAll()
     {
       return _toys;
+    }
+    private string GetPicture(int id)
+    {
+      if (id == 5)
+      {
+        return "http://bogleech.com/pokemon/allpokes/002Ivysaur.png";
+      }
+      else if (id == 4)
+      {
+        return "https://www.seeklogo.net/wp-content/uploads/2013/04/pokemon-anime-vector.png";
+      }
+      else if (id == 3)
+      {
+        return "https://i.pinimg.com/originals/31/4c/95/314c95fd708956690b48d473e94aea29.jpg";
+      }
+      else if (id == 2)
+      {
+        return "http://quovx4d83tr2hp1r22mgwa1m.wpengine.netdna-cdn.com/wp-content/uploads/2016/07/bewear-400x400.png";
+      }
+      else
+      {
+        return "https://assetsds.cdnedge.bluemix.net/sites/default/files/styles/big_6/public/news/images/pokemon_02.jpg?itok=dIUYs4TX";
+      }
     }
 
   }
